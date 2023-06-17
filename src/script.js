ff4a5b2d40c4c35b3457c2016cb89d16c62bd90c
Tumelo Mahlangu <tumie201511@gmail.com>
2023-06-18 01:52:48 +0200

    updated html and css files

--- src/script.js
+++ src/script.js
@@ -1,68 +1,45 @@
 const hamburger = document.querySelector(".hamBurger");
 const navigationMenu = document.querySelector(".nav__list");
+const navigationItems = document.getElementsByClassName("collapse");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navigationMenu.classList.toggle("active")
 });
 
-document.querySelectorAll(".nav__item").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navigationMenu.classList.remove("active");
 }));
 
-const isDisplayBlock = (items) => {
+function isDisplayBlock(items) {
     return items.style.display === "block" ? items.style.display = "none" : items.style.display = "block";
 }
 
-const Collapsibles = (screenSize) => {
-    let navigationItems = document.getElementsByClassName("collapse");
-    if(screenSize.matches){
-        for(let i=0; i<navigationItems.length; i++){
-            navigationItems[i].addEventListener("click", function(){
-                this.classList.toggle("collapse");
-                let content = this.nextElementSibling;
-                if (content.style.maxHeight){
-                    content.style.maxHeight = null;
-                } else {
-                    content.style.maxHeight = content.scrollHeight + "px";
-                }
-            });
-        }
+function collapseNavigationItems(){
+    for(let j=0; j<navigationItems.length; j++){
+        navigationItems[j].addEventListener("click", () => {
+            const content = document.querySelector('.content');
+            if(content.style.display === "none"){
+                content.style.display = "block";
+            } else{
+                content.style.display = "none";
+            }
+        })
     }
 }
 
-let screenWidth = window.matchMedia("(max-width: 745px)");
-Collapsibles(screenWidth);
-screenWidth.addEventListener(Collapsibles);
+function handleScreenChange(mediaQuery){
+    if(mediaQuery.matches){
+        collapseNavigationItems();
+    }
+}
 
-const swiper = new Swiper(".slide-content", {
-    slidesPerView: 2,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+const mediaQuery = window.matchMedia('(max-width: 745px)');
+handleScreenChange(mediaQuery);
+mediaQuery.addEventListener("change", function(event) {
+    handleScreenChange(event.target)
+})
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
 
